c6bfefb57331b6bf00dc1049952e8fee40f69077
Christian Atallah <c.atallah2@newcastle.ac.uk>
2018-09-07 14:05:17 +0100

    initial commit

---
+++ Move.py
@@ -0,0 +1,8 @@
+
+class Move:
+
+    def __init__(self, old_sol, path, new_sol):
+
+        self.old_sol = old_sol
+        self.path = path
+        self.new_sol = new_sol
---
+++ Solution.py
@@ -0,0 +1,8 @@
+
+class Solution:
+
+    def __init__(self, val, fitness):
+
+        self.val = val
+
+        self.fitness = fitness
