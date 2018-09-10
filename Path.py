4d5688b7d25f2f760489de2844bc2b02bd842296
Christian Atallah <c.atallah2@newcastle.ac.uk>
2018-09-10 13:33:10 +0100

    Added some abstract methods, best_score method, and a draft of the run method

--- Path.py
+++ Path.py
@@ -2,11 +2,10 @@
 class Path:
 
     def __init__(self, movetype, change):
-
         self._MOVETYPES = ['single_swap', 'single_change']
         self.change = change
 
         if movetype not in self._MOVETYPES:
             raise ValueError('Only types accepted are %s.' % (', '.join(self._MOVETYPES)))
-            
+
         self.movetype = movetype
--- TabuSearch.py
+++ TabuSearch.py
@@ -1,14 +1,55 @@
 from TabuList import TabuList
+from abc import abstractmethod
 
 class TabuSearch:
 
-    def __init__(self, initial_solution, max_len, list_type, max_tenure):
+    def __init__(self, initial_solution, max_len, list_type, max_tenure, max_steps):
         self.curr_sol = initial_solution
         self.tabu_list = TabuList(max_len, list_type, max_tenure)
+        self.max_steps = max_steps
+        self.best = ''
         # self.move_manager = MoveManager()
 
 
-    # create_neighbourhood()
-    # score()
-    # best_score()
-    # run()
+    @abstractmethod
+    def _create_neighbourhood(self):
+        """
+            take self.curr_sol and produce a list of moves/paths
+        """
+        pass
+
+    @abstractmethod
+    def _score(self):
+        pass
+
+    def _best_score(self, neighbourhood):
+        return neighbourhood[argmax([self._score(x) for x in neighbourhood])]
+
+
+    def run(self):
+        for i in range(0, self.max_steps):
+            neighbourhood = self._create_neighbourhood()
+            neighbourhood_best = self._best_score(neighbourhood)
+
+            while True:
+                if neighbourhood_best in self.tabu_list:
+                    if self._score(neighbourhood_best) > self._score(self.best):
+                        self.tabu_list.append_tabu_list(neighborhood_best)
+                        self.best = neighbourhood_best
+                        break
+
+                    else:
+                        neighbourhood.remove(neighbourhood_best)
+                        neighbourhood_best = self._best_score(neighbourhood)
+
+                else:
+                    self.tabu_list.append_tabu_list(neighbourhood_best)
+                    self.curr_sol = neighbourhood_best
+
+                    if self._score(self.current) > self._score(self.best):
+                        self.best = self.current
+
+                    break
+
+        print 'RECHED MAX STEPS'
+        return self.best, self._score(self.best)
